ab8b129cfd011591ddc199b96733c68dd79259a4
admin <clayton.collins@eyecuelab.com>
2019-06-03 20:53:09 -0700

    add html sections

--- scripts.js
+++ scripts.js
@@ -5,11 +5,13 @@ let segLength = 80,
   y2,
   pointArray = [],
   gaussianArray = [],
-  neuronGauss = 60,
+  neuronGauss = 40,
   padding = neuronGauss * 2,
   unitX,
   unitY,
   minNeuronDistance = 300,
+  rowNeurons = 5,
+  columnNeurons = 5,
   segments = 6,
   detail = false,
   selected = 5,
@@ -18,11 +20,12 @@ let segLength = 80,
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  unitX = Math.floor((windowWidth - (padding * 2)) / 4);
-  unitY = Math.floor((windowHeight - (padding * 2)) / 4);
+  unitX = Math.floor((windowWidth - (padding * 2)) / rowNeurons);
+  unitY = Math.floor((windowHeight - (padding * 2)) / columnNeurons);
   createPointArray();
   createGaussianArray();
-  noCursor();
+  textFont('Roboto');
+  // noCursor();
 }
 
 function draw() {
@@ -100,55 +103,85 @@ function words() {
   textSize(16);
   fill(0,0,0,(2 - canvasScale) * 255)
   if (detail) {
-    text('JBM', pointArray[5].x, pointArray[5].y)
-    text('PAPERS', pointArray[6].x, pointArray[6].y)
-    text('BIOGRAPHY', pointArray[9].x, pointArray[9].y)
-    text('LINKS', pointArray[10].x, pointArray[10].y)
+    text('JBM', pointArray[6].x, pointArray[6].y)
+    text('RESEARCH', pointArray[7].x, pointArray[7].y)
+    text('PAPERS', pointArray[8].x, pointArray[8].y)
+    text('DOCUMENTS', pointArray[11].x, pointArray[11].y)
+    text('TWITTER', pointArray[12].x, pointArray[12].y)
+    text('GITHUB', pointArray[13].x, pointArray[13].y)
 
     switch (selected) {
-      case 5:
-        fill(0,0,0,255);
-        text('JBM', pointArray[5].x, pointArray[5].y);
-        break;
       case 6:
         fill(0,0,0,255);
-        text('PAPERS', pointArray[6].x, pointArray[6].y)
+        text('JBM', pointArray[6].x, pointArray[6].y);
         break;
-      case 9:
+      case 7:
         fill(0,0,0,255);
-        text('BIOGRAPHY', pointArray[9].x, pointArray[9].y)
+        text('RESEARCH', pointArray[7].x, pointArray[7].y)
         break;
-      case 10:
+      case 8:
         fill(0,0,0,255);
-        text('LINKS', pointArray[10].x, pointArray[10].y)
+        text('PAPERS', pointArray[8].x, pointArray[8].y)
+        break;
+      case 11:
+        fill(0,0,0,255);
+        text('DOCUMENTS', pointArray[11].x, pointArray[11].y)
+        break;
+      case 12:
+        fill(0,0,0,255);
+        text('TWITTER', pointArray[12].x, pointArray[12].y)
+        break;
+      case 12:
+        fill(0,0,0,255);
+        text('GITHUB', pointArray[13].x, pointArray[13].y)
         break;
     }
   } else {
-    text('JBM', pointArray[5].x, pointArray[5].y)
-    text('PAPERS', pointArray[6].x, pointArray[6].y)
-    text('BIOGRAPHY', pointArray[9].x, pointArray[9].y)
-    text('LINKS', pointArray[10].x, pointArray[10].y)
+    text('JBM', pointArray[6].x, pointArray[6].y)
+    text('RESEARCH', pointArray[7].x, pointArray[7].y)
+    text('PAPERS', pointArray[8].x, pointArray[8].y)
+    text('DOCUMENTS', pointArray[11].x, pointArray[11].y)
+    text('TWITTER', pointArray[12].x, pointArray[12].y)
+    text('GITHUB', pointArray[13].x, pointArray[13].y)
   }
 }
 
 function mousePressed() {
   if (detail === false) {
-    if (mouseX < (pointArray[5].x + 100) && mouseX > (pointArray[5].x - 25) && mouseY < (pointArray[5].y + 10) && mouseY > (pointArray[5].y - 25)) {
+    if (mouseX < (pointArray[6].x + 100) && mouseX > (pointArray[6].x - 25) && mouseY < (pointArray[6].y + 10) && mouseY > (pointArray[6].y - 25)) {
       detail = true;
-      selected = 5;
-    } else if (mouseX < (pointArray[6].x + 100) && mouseX > (pointArray[6].x - 25) && mouseY < (pointArray[6].y + 10) && mouseY > (pointArray[6].y - 25)) {
-      detail = true;
+      document.getElementById("bio").style.visibility = "visible";
+      document.getElementById("bio").style.opacity = 1;
       selected = 6;
-    } else if (mouseX < (pointArray[9].x + 100) && mouseX > (pointArray[9].x - 25) && mouseY < (pointArray[9].y + 10) && mouseY > (pointArray[9].y - 25)) {
+    } else if (mouseX < (pointArray[7].x + 100) && mouseX > (pointArray[7].x - 25) && mouseY < (pointArray[7].y + 10) && mouseY > (pointArray[7].y - 25)) {
       detail = true;
-      selected = 9;
-    } else if (mouseX < (pointArray[10].x + 100) && mouseX > (pointArray[10].x - 25) && mouseY < (pointArray[10].y + 10) && mouseY > (pointArray[10].y - 25)) {
+      document.getElementById("research").style.visibility = "visible";
+      document.getElementById("research").style.opacity = 1;
+      selected = 7;
+    } else if (mouseX < (pointArray[8].x + 100) && mouseX > (pointArray[8].x - 25) && mouseY < (pointArray[8].y + 10) && mouseY > (pointArray[8].y - 25)) {
       detail = true;
-      selected = 10;
+      document.getElementById("papers").style.visibility = "visible";
+      document.getElementById("papers").style.opacity = 1;
+      selected = 8;
+    } else if (mouseX < (pointArray[11].x + 100) && mouseX > (pointArray[11].x - 25) && mouseY < (pointArray[11].y + 10) && mouseY > (pointArray[11].y - 25)) {
+      detail = true;
+      document.getElementById("documents").style.visibility = "visible";
+      document.getElementById("documents").style.opacity = 1;
+      selected = 11;
+    } else if (mouseX < (pointArray[12].x + 100) && mouseX > (pointArray[12].x - 25) && mouseY < (pointArray[12].y + 10) && mouseY > (pointArray[12].y - 25)) {
+      window.location.href = "https://twitter.com/seanfrancismcn";
+    } else if (mouseX < (pointArray[13].x + 100) && mouseX > (pointArray[13].x - 25) && mouseY < (pointArray[13].y + 10) && mouseY > (pointArray[13].y - 25)) {
+      window.location.href = "https://github.com/jbmelander";
     }
   } else {
     if (mouseX < 150 && mouseY > -100) {
       detail = false;
+      var x = document.getElementsByClassName("section");
+      var i;
+      for (i = 0; i < x.length; i++) {
+        x[i].style.visibility = "none";
+        x[i].style.opacity = "0";
+      }
       loop();
       noCursor();
     }
@@ -157,16 +190,16 @@ function mousePressed() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  unitX = Math.floor((windowWidth - (padding * 2)) / 4);
-  unitY = Math.floor((windowHeight - (padding * 2)) / 4);
+  unitX = Math.floor((windowWidth - (padding * 2)) / rowNeurons);
+  unitY = Math.floor((windowHeight - (padding * 2)) / columnNeurons);
   createPointArray();
   createGaussianArray();
 }
 
 function createPointArray() {
   pointArray = [];
-  for (var i = 0; i < 4; i++) {
-    for (var j = 0; j < 4; j++) {
+  for (var i = 0; i < rowNeurons; i++) {
+    for (var j = 0; j < columnNeurons; j++) {
       pointArray.push(createVector(((i*unitX) + unitX/2 + randomGaussian(0, neuronGauss) + padding), ((j*unitY) + unitY/2 + randomGaussian(0, neuronGauss) + padding)));
     }
   }
@@ -174,8 +207,8 @@ function createPointArray() {
 
 function createGaussianArray() {
   gaussianArray = [];
-  for (var i = 0; i < 4; i++) {
-    for (var j = 0; j < 4; j++) {
+  for (var i = 0; i < rowNeurons; i++) {
+    for (var j = 0; j < columnNeurons; j++) {
       for(var k= 0; k < segments; k++) {
         gaussianArray.push(randomGaussian(0, 5));
       }
